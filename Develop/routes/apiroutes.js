579354bfd12cae69ab34281219ad916e3f37aced
Jordan Pace <jordanpace95@gmail.com>
2023-05-09 12:19:06 -0600

    adds delete route

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -4,14 +4,14 @@ const { v4: uuidv4 } = require("uuid");
 
 router.get("/notes", (req, res) => {
     fs.readFile("db/db.json", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      res.json(JSON.parse(data));
+        if (err) {
+            console.log(err);
+        }
+        res.json(JSON.parse(data));
     });
-  });
+});
 
-  router.post("/notes", (req, res) => {
+router.post("/notes", (req, res) => {
 
     console.info(`${req.method} request received to save note`);
 
@@ -19,33 +19,49 @@ router.get("/notes", (req, res) => {
 
     if (title && text) {
 
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(), 
-        // add unique id to each note
-      };
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+            // add unique id to each note
+        };
 
-      fs.readFile(`./db/db.json`, "utf8", (err, data) => {
-        if (err) {
-          console.error("read error: " + err);
-        } else {
-          const notesArr = JSON.parse(data);
-          notesArr.push(newNote);
-          const notesString = JSON.stringify(notesArr, null, 4);
-          fs.writeFile(`./db/db.json`, notesString, (err) =>
-            err
-              ? console.error(err)
-              : console.log(
-                  `Note titled '${newNote.title}' has been written to JSON file`
-                )
-          );
-        }
-      });
-      res.status(200).json("Success!");
+        fs.readFile(`./db/db.json`, "utf8", (err, data) => {
+            if (err) {
+                console.error("read error: " + err);
+            } else {
+                const notesArr = JSON.parse(data);
+                notesArr.push(newNote);
+                const notesString = JSON.stringify(notesArr, null, 4);
+                fs.writeFile(`./db/db.json`, notesString, (err) =>
+                    err
+                        ? console.error(err)
+                        : console.log(
+                            `Okay, I'll write that down...'${newNote.title}' `
+                        )
+                );
+            }
+        });
+        res.status(200).json("Success!");
     } else {
-      res.status(500).json("Error saving note");
+        res.status(500).json("Error saving note");
     }
-  });
+});
+
+router.delete("/notes/:id",
+    (req, res) => {
+        console.info(`Bad notes get the ${req.method} request >:(`);
+        const noteId = req.params.id;
+        const db = JSON.parse(fs.readFileSync("db/db.json", "utf-8")).filter(({ id }) => id !== noteId);
+        const noteString = JSON.stringify(db);
+        fs.writeFile(`db/db.json`, noteString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(
+                    'Great success!'
+                )
+        );
+        res.json(db);
+    });
 
 module.exports = router;
